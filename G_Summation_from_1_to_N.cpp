0e7b9256a3cd9b4b01c9dc95adaeda174f52a915
Shakhawat Hossain Shuvo <65659322+shakhawat2020@users.noreply.github.com>
2025-12-19 18:15:16 +0600

    Add files via upload

---
+++ G_Summation_from_1_to_N.cpp
@@ -0,0 +1,13 @@
+// problem link: https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/G
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+   long long int n,sum;
+    cin >> n;
+
+    sum = (n*(n+1))/2;
+    cout << sum << endl;
+    return 0;
+}
